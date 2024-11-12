7eebb077198a14480548c4e45671312a86b6b5de
zzvezdomirov <zaharizvezdomirov@gmail.com>
2024-11-12 15:10:14 +0200

    Initial commit - Spring boot project setup.

---
+++ coordinatedhttprequests/src/main/java/com/sap/coordinatedhttprequests/CoordinatedHttpRequestsApplication.java
@@ -0,0 +1,13 @@
+package com.sap.coordinatedhttprequests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoordinatedHttpRequestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoordinatedHttpRequestsApplication.class, args);
+	}
+
+}
---
+++ coordinatedhttprequests/src/test/java/com/sap/coordinatedhttprequests/CoordinatedHttpRequestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.coordinatedhttprequests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoordinatedHttpRequestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
